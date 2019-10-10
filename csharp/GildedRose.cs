9b17ca252bfeeda46e7de5b180f2cf8876f309f2
Rafael Barreto <rafaelbarretoo2008@hotmail.com>
2019-10-09 23:15:33 -0300

    master - Refactoring of decreasing quality of conjured items.

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -14,14 +14,20 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+
+                if (Items[i].Name != "Aged Brie"
+                      && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert"
+                      && Items[i].Quality > 0)
                 {
-                    if (Items[i].Quality > 0)
+                    if (Items[i].Name.Contains("Conjured"))
                     {
+                        Items[i].Quality -= 2;
+                    }
+
+                    else
                         if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
+                    {
+                        Items[i].Quality = Items[i].Quality - 1;
                     }
                 }
                 else
